fbd53af1887758310f243d749b69bdbfebd8e098
NeoCat98 <00076017@uca.edu.sv>
2018-09-28 20:01:19 -0600

    primer commit

--- ejercicio_12.js
+++ ejercicio_12.js
@@ -1,25 +1,22 @@
-array=[
-    {
-        nombre: 'Juan',
-        edad: 13, 
-    }
+array=[{nombre: 'Juan',
+        edad: 13}
 ];
 function ingresar(persona){
-    array.push(nombre);
+    array.push(persona);
     console.log('Se ha agregado el nombre');
 }
-function buscar(persona){
+function buscar(nombre){
     cnts = 0;
     encontro = false;
     for (i of array) {
-        if (array.nombre == persona.nombre) {
+        if (i.nombre == nombre) {
             position = cnts;
             encontro = true;
         }
         cnts++;
     }
     if(true){
-        console.log('La persona se encontro'+persona);
+        console.log('La persona se encontro en la posicion: ');
         return cnts;
     }
     else{
@@ -27,19 +24,73 @@ function buscar(persona){
     }
 }
 
-function Modificar(persona){
-
+function Modificar(nombre2){
+    for (var i=0; i<array.length;i++){
+        if(array[i].nombre=nombre2){
+            array[i].nombre = prompt('Nuevo nombre');
+            array[i].edad = prompt('Nueva edad');
+        }
+    }
     console.log('Persona modificada');
 }
 
-function Eliminar(persona){
-    position = buscar(persona);
-    delete(array,position);
-    console.log('Persona eliminada');
+function Eliminar(nombre){
+    cnts=0;
+    encontro=false;
+    for (i of array) {
+        if (i.nombre == nombre) {
+            position = cnts;
+            encontro=true;
+        }
+        cnts++;
+    }
+    if(encontro){
+        array.splice(position,1);
+        console.log('Persona eliminada');
+    }
+    else{
+        console.log('No se encontro a la persona');
+    }
+
 }
 
-function Mostar(){
+function Mostrar(){
     for(i of array){
         console.log(i);
+    }
+}
+
+function Inicio(){
+    n=0;
+    while (n!=1) {
+        opcion=prompt('1.INGRESAR PERSONAR\n2.BUSCAR PERSONA\n3.MODIFICAR PERSONA\n4.ELIMINAR PERSONA\n5.ELIMINAR PERSONA\n6.SALIR');
+        if(opcion==1){
+            persona = prompt('Ingrese a la persona ({nombre: ... edad: ...})');
+            ingresar(persona);
+            break;
+        }
+        else if(opcion==2){
+            nombre = prompt('Ingrese el nombre');
+            buscar(nombre);
+            break;
+        }
+        else if(opcion==3){
+            nombre = prompt('Ingrese el nombre');
+            Modificar(nombre);
+            break;
+        }
+        else if(opcion==4){
+            nombre = prompt('Ingrese el nombre');
+            Eliminar(nombre);
+            break;
+        }
+        else if(opcion==5){
+            Mostar();
+            break;
+        }
+        else if(opcion==6){
+            n=1;
+            break;
+        }
     }
 }
--- ejercicio_6.js
+++ ejercicio_6.js
@@ -1,36 +1,38 @@
 function clave(numero){
-    cadena = '';
-    for (let n=0;n<numero.length;n++) {
-        switch(numero[n]){
-            case 0:
-                cadena=cadena.concat('m');
+    num = numero.toString();
+    cadena="";
+    for (let n=0;n<num.length;n++) {
+        console.log(num[n]);
+        switch(num[n]){
+            case "0":
+                cadena=cadena+"m";
                 break;
-            case 1:
-                cadena=cadena.concat('u');
+            case "1":
+                cadena=cadena+"u";
                 break;
-            case 2:
-                cadena=cadena.concat('r');
+            case "2":
+                cadena=cadena+"r";
                 break;
-            case 3:
-                cadena=cadena.concat('c');
+            case "3":
+                cadena=cadena+"c";
                 break;
-            case 4:
-                cadena=cadena.concat('i');
+            case "4":
+                cadena=cadena+"i";
                 break;
-            case 5:
-                cadena=cadena.concat('e');
+            case "5":
+                cadena=cadena+"e";
                 break;
-            case 6:
-                cadena=cadena.concat('l');
+            case "6":
+                cadena=cadena+"l";
                 break;
-            case 7:
-                cadena=cadena.concat('a');
+            case "7":
+                cadena=cadena+"a";
                 break;
-            case 8:
-                cadena=cadena.concat('g');
+            case "8":
+                cadena=cadena+"g";
                 break;
-            case 9:
-                cadena=cadena.concat('o');
+            case "9":
+                cadena=cadena+"o";
                 break;
         }
     }
